9770bd0ca1b44aa1ca3b916d916509f435c9b1e3
tkinter <interface>
2019-05-05 17:45:53 +0000

    basic

--- comp_image_draw.py
+++ comp_image_draw.py
@@ -1,15 +1,19 @@
 import numpy as np
 import tkinter as tk
 
-
-
+WIDTH, HEIGHT = 800, 600
+buttons = []
 
 def main():
 	root = tk.Tk()
-	cv = tk.Canvas(root, bd=5, bg='gray')
+	cv = tk.Canvas(root, width=WIDTH, height=HEIGHT, bd=5, bg='gray')
 	cv.pack()
 	
-	draw_area = tk.Canvas(root)
+	draw_area = tk.Canvas(root, relx=.1, rely=.1, width=280, height=280, bg='white')
+	
+	global buttons
+	for i in range(10):
+		b = tk.Button(root, relx=.7, rely=.09*i, relwidth=.2, relheight=.1, bg='gray')
 	
 	
 	root.mainLoop()
