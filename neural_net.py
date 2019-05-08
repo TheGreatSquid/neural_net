c33967e2a7cd1a0a1e19a5a8026bff0cdd85e580
TheGreatSquid <50186460+TheGreatSquid@users.noreply.github.com>
2019-05-08 10:15:28 -0700

    Slight reformat, factored out `test` function

--- neural_net.py
+++ neural_net.py
@@ -12,16 +12,30 @@ import os
 Datum = nt('Datum', 'inputs, targets')
 
 
+def test(brain, target):
+	try:
+		test_img = Image.open(f'test_{target}.png')
+	except:
+		print(f'Test image for {target} does not exist.')
+	
+	test_in = list(np.array(test_img.getdata()) / 255)
+	print(f'Testing number: {target}')
+	out = brain.feed_forward(test_in)
+	prediction = np.where(out == max(out))
+	print(f'Brain thinks this is a: {prediction[0]}')
+
+
 def main():
 	brain = nn.NeuralNetwork(784, 50, 10)
-	data_s = time.time()
+	data_start = time.time()
 	
 	img_data = []
 	for subdir, dirs, files in os.walk('training_images/'):
+		last_char = subdir[-1]
 		# just for testing purposes
-		if subdir[-1] == '/':
+		if last_char == '/':
 			continue
-		if int(subdir[-1]) > 6:
+		if int(last_char) > 6:
 			continue
 		for file in files:
 			if file == 'readme.txt':
@@ -29,37 +43,25 @@ def main():
 			p = os.path.join(subdir, file)
 			img = Image.open(p)
 			inputs = np.array(img.getdata()) / 255
-			targets = [0 for _ in range(10)]
-			targets[int(subdir[-1])] = 1
+			targets = [0] * 10
+			targets[int(last_char)] = 1
 			d = Datum(brain.prepare(inputs), brain.prepare(targets))
 			img_data.append(d)
 	
-	data_e = time.time()
-	print(data_e - data_s)
+	data_end = time.time()
+	print(f'Time to parse data: {data_end - data_start}')
 	
-	start = time.time()
+	train_start = time.time()
 	# train
 	for _ in range(10000):
 		datum = rd.choice(img_data)
 		brain.train(datum.inputs, datum.targets)
 	
+	train_end = time.time()
+	print(f'Time to train: {train_end - train_start}')
 	# test
-	test_img = Image.open('test_1.png')
-	test_in = list(np.array(test_img.getdata()) / 255)
-	print('test 1')
-	out = brain.feed_forward(test_in)
-	prediction = np.where(out == max(out))
-	print(prediction[0])
-	
-	test_img = Image.open('test_6.png')
-	test_in = list(np.array(test_img.getdata()) / 255)
-	print('test 6')
-	out = brain.feed_forward(test_in)
-	prediction = np.where(out == max(out))
-	print(prediction[0])
-	
-	end = time.time()
-	print(end - start)
+	test(brain, 1)
+	test(brain, 6)
 	
 
 if __name__ == '__main__': 
